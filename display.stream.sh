202c3a3513e639d7271f138f4a945a2d595bab8c
displaystream <54572667+displaystream@users.noreply.github.com>
2022-09-22 13:25:03 -0500

    Update display.stream.sh

--- display.stream.sh
+++ display.stream.sh
@@ -9,4 +9,4 @@ unclutter -idle 0.5 -root &
 sed -i ‘s/”exited_cleanly”:false/”exited_cleanly”:true/’ /home/$USER/.config/chromium/Default/Preferences
 sed -i ‘s/”exit_type”:”Crashed”/”exit_type”:”Normal”/’ /home/$USER/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --kiosk --disable-infobars --noerrdialogs --app https://app.display.stream &
+/usr/bin/chromium-browser --kiosk --disable-infobars --noerrdialogs --autoplay-policy=no-user-gesture-required --app https://app.display.stream &
